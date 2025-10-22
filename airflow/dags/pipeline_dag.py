517e58c2a77da7d22ff701744e51e55533867c93
FenosoaSolofoniaina <solofoniainafenosoa020@gmail.com>
2025-10-22 18:15:06 +0300

    Fix bug on airflow_container

--- airflow/dags/pipeline_dag.py
+++ airflow/dags/pipeline_dag.py
@@ -1,65 +1,60 @@
+import os
+from dotenv import load_env
+from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.docker.operators.docker import DockerOperator
-from docker.types import Mount
-from datetime import datetime, timedelta
 
 
 
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2025, 10, 10),
-    'retries': 1
+    'retries': 1,
+    'retries_delay' : timedelta(minutes=5)
 }
 
 with DAG(
     dag_id='pipeline_les_petites_jupes_de_prunes_v1',
     description='Run pipeline that extract data in the website to push it on the cloud',
     default_args=default_args,
-    schedule=timedelta(minutes=30),
+    schedule=None,
     catchup=False
 ) as dag:
 
+    load_env()
+
+    DOCKER_URL = 'unix://var/run/docker.sock'
+    PROJECT_PREFIX = os.getenv('PROJECT_PREFIX')
+    
+    # Task 1
     init_wf = BashOperator(
         task_id='bash_model_initializing',
         bash_command='echo "INITIALIZING THE WORKFLOW ..."'
     )
 
+    # Task 2
     scrape_wf = DockerOperator(
         task_id='docker_model_scrape_data',
-        docker_url='unix://var/run/docker.sock',
-        image='web_scraper_app:latest',
-        container_name='web_scraper_app',
-        mounts=[
-            Mount(source='./../../scraper',
-                  target='/app/scraper',
-                  type='bind')
-        ],
+        docker_url=DOCKER_URL,
+        image=f'{PROJECT_PREFIX}-scraper:latest',
+        container_name='web_scraper_container',
         command=["python3", "extract_data.py"],
-        #auto_remove=True,
-        network_mode='bridge',
-        dag=dag
+        network_mode='bridge'
     )
 
+    # Task 3
     dbt_wf = DockerOperator(
         task_id='docker_model_dbt_transformation',
-        docker_url='unix://var/run/docker.sock',
-        image='dbt_app:latest',
-        container_name='dbt_app',
-        mounts = [
-            Mount(source='./../../dbt_part',
-                  target='/app/dbt_part',
-                  type='bind'),
-            Mount(source='./../../dbt_part/.dbt/profiles.yml',
-                  target='/app/dbt_part/.dbt/profiles.yml',
-                  type='bind')
-        ],
+        docker_url=DOCKER_URL,
+        image=f'{PROJECT_PREFIX}-dbt:latest',
+        container_name='dbt_container',
         command=["dbt", "run", "--profiles-dir", "/app/dbt_part/.dbt"],
-        #auto_remove=True,
         network_mode='bridge',
-        dag=dag
     )
 
+    # Task 4
     end_wf = BashOperator(
         task_id='bash_model_ending',
         bash_command='echo "CLOSING THE WORKFLOW..."'
